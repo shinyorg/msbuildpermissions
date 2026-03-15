78c9e35d0cd61d0de4199a3b48ccb6c0ecf63a6f
Allan Ritchie <allan.ritchie@gmail.com>
2026-03-15 01:38:03 -0400

    WIP

--- tests/Shiny.Permissions.MSBuild.Tests/AndroidManifestFeatureGeneratorTests.cs
+++ tests/Shiny.Permissions.MSBuild.Tests/AndroidManifestFeatureGeneratorTests.cs
@@ -5,162 +5,62 @@ namespace Shiny.Permissions.MSBuild.Tests;
 
 public class AndroidManifestFeatureGeneratorTests
 {
-    public class ResolveNameTests
+    [Fact]
+    public Task Empty()
     {
-        [Theory]
-        [InlineData("android.hardware.LOCATION.GPS", "android.hardware.LOCATION.GPS")]
-        [InlineData("some.namedfeature.HELLO", "some.namedfeature.HELLO")]
-        [InlineData("com.example.custom.FEATURE", "com.example.custom.FEATURE")]
-        public void FullyQualified_ThreeOrMoreSegments_ReturnedAsIs(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestFeatureGenerator.ResolveName(input));
-        }
-
-        [Theory]
-        [InlineData("CAMERA", "android.hardware.CAMERA")]
-        [InlineData("BLUETOOTH", "android.hardware.BLUETOOTH")]
-        public void SingleSegment_PrefixedWithAndroidHardware(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestFeatureGenerator.ResolveName(input));
-        }
-
-        [Theory]
-        [InlineData("LOCATION.NETWORK", "android.hardware.LOCATION.NETWORK")]
-        [InlineData("LOCATION.GPS", "android.hardware.LOCATION.GPS")]
-        [InlineData("a.b", "android.hardware.a.b")]
-        public void TwoSegments_StillPrefixed(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestFeatureGenerator.ResolveName(input));
-        }
-
-        [Fact]
-        public void EmptyString_PrefixedWithAndroidHardware()
-        {
-            Assert.Equal("android.hardware.", AndroidManifestFeatureGenerator.ResolveName(""));
-        }
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml([]));
     }
 
-    public class GenerateElementTests
+    [Fact]
+    public Task SingleShortName()
     {
-        [Fact]
-        public void WithRequiredTrue()
+        var features = new[]
         {
-            var feature = new AndroidFeature
-            {
-                Name = "android.hardware.LOCATION.GPS",
-                Required = "true"
-            };
-            var xml = AndroidManifestFeatureGenerator.GenerateElement(feature);
-
-            Assert.Equal(
-                "<uses-feature android:name=\"android.hardware.LOCATION.GPS\" android:required=\"true\" />",
-                xml
-            );
-        }
-
-        [Fact]
-        public void WithRequiredFalse()
-        {
-            var feature = new AndroidFeature
-            {
-                Name = "LOCATION.NETWORK",
-                Required = "false"
-            };
-            var xml = AndroidManifestFeatureGenerator.GenerateElement(feature);
-
-            Assert.Equal(
-                "<uses-feature android:name=\"android.hardware.LOCATION.NETWORK\" android:required=\"false\" />",
-                xml
-            );
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("   ")]
-        public void NullOrWhitespace_Required_OmittedFromOutput(string? value)
-        {
-            var feature = new AndroidFeature
-            {
-                Name = "CAMERA",
-                Required = value
-            };
-            var xml = AndroidManifestFeatureGenerator.GenerateElement(feature);
-
-            Assert.DoesNotContain("android:required", xml);
-            Assert.Equal(
-                "<uses-feature android:name=\"android.hardware.CAMERA\" />",
-                xml
-            );
-        }
-
-        [Fact]
-        public void NameResolution_AppliedInElement()
-        {
-            var feature = new AndroidFeature { Name = "CAMERA", Required = "true" };
-            var xml = AndroidManifestFeatureGenerator.GenerateElement(feature);
-
-            Assert.Contains("android:name=\"android.hardware.CAMERA\"", xml);
-        }
+            new AndroidFeature { Name = "CAMERA", Required = "true" },
+        };
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml(features));
     }
 
-    public class GenerateManifestXmlTests
+    [Fact]
+    public Task SingleFullyQualifiedName()
     {
-        [Fact]
-        public void MatchesSpecExample()
+        var features = new[]
         {
-            var features = new[]
-            {
-                new AndroidFeature { Name = "android.hardware.LOCATION.GPS", Required = "true" },
-                new AndroidFeature { Name = "LOCATION.NETWORK", Required = "false" },
-                new AndroidFeature { Name = "some.namedfeature.HELLO", Required = "true" },
-            };
+            new AndroidFeature { Name = "android.hardware.LOCATION.GPS", Required = "true" },
+        };
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml(features));
+    }
 
-            var xml = AndroidManifestFeatureGenerator.GenerateManifestXml(features);
-
-            Assert.Contains("<manifest>", xml);
-            Assert.Contains("</manifest>", xml);
-            Assert.Contains("<uses-feature android:name=\"android.hardware.LOCATION.GPS\" android:required=\"true\" />", xml);
-            Assert.Contains("<uses-feature android:name=\"android.hardware.LOCATION.NETWORK\" android:required=\"false\" />", xml);
-            Assert.Contains("<uses-feature android:name=\"some.namedfeature.HELLO\" android:required=\"true\" />", xml);
-        }
-
-        [Fact]
-        public void EmptyCollection_ValidManifestShell()
+    [Fact]
+    public Task RequiredFalse()
+    {
+        var features = new[]
         {
-            var xml = AndroidManifestFeatureGenerator.GenerateManifestXml([]);
+            new AndroidFeature { Name = "LOCATION.NETWORK", Required = "false" },
+        };
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml(features));
+    }
 
-            Assert.Contains("<manifest>", xml);
-            Assert.Contains("</manifest>", xml);
-            Assert.DoesNotContain("<uses-feature", xml);
-        }
-
-        [Fact]
-        public void SingleFeature_ValidOutput()
+    [Fact]
+    public Task RequiredOmitted()
+    {
+        var features = new[]
         {
-            var features = new[]
-            {
-                new AndroidFeature { Name = "CAMERA", Required = "true" },
-            };
+            new AndroidFeature { Name = "CAMERA" },
+        };
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml(features));
+    }
 
-            var xml = AndroidManifestFeatureGenerator.GenerateManifestXml(features);
-
-            Assert.Contains("<uses-feature android:name=\"android.hardware.CAMERA\" android:required=\"true\" />", xml);
-        }
-
-        [Fact]
-        public void Elements_AreIndentedWithFourSpaces()
+    [Fact]
+    public Task MultipleFeatures_MixedNames()
+    {
+        var features = new[]
         {
-            var features = new[]
-            {
-                new AndroidFeature { Name = "CAMERA", Required = "true" },
-            };
-
-            var xml = AndroidManifestFeatureGenerator.GenerateManifestXml(features);
-            var lines = xml.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            var featureLine = lines.First(l => l.Contains("<uses-feature"));
-
-            Assert.StartsWith("    <uses-feature", featureLine);
-        }
+            new AndroidFeature { Name = "android.hardware.LOCATION.GPS", Required = "true" },
+            new AndroidFeature { Name = "LOCATION.NETWORK", Required = "false" },
+            new AndroidFeature { Name = "some.namedfeature.HELLO", Required = "true" },
+            new AndroidFeature { Name = "CAMERA" },
+        };
+        return Verify(AndroidManifestFeatureGenerator.GenerateManifestXml(features));
     }
 }
--- tests/Shiny.Permissions.MSBuild.Tests/AndroidManifestPermissionGeneratorTests.cs
+++ tests/Shiny.Permissions.MSBuild.Tests/AndroidManifestPermissionGeneratorTests.cs
@@ -5,193 +5,72 @@ namespace Shiny.Permissions.MSBuild.Tests;
 
 public class AndroidManifestPermissionGeneratorTests
 {
-    public class ResolveNameTests
+    [Fact]
+    public Task Empty()
     {
-        [Theory]
-        [InlineData("android.permission.READ_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE")]
-        [InlineData("some.namedpermissions.ACCESS", "some.namedpermissions.ACCESS")]
-        [InlineData("com.example.myapp.CUSTOM", "com.example.myapp.CUSTOM")]
-        public void FullyQualified_ThreeOrMoreSegments_ReturnedAsIs(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestPermissionGenerator.ResolveName(input));
-        }
-
-        [Theory]
-        [InlineData("WRITE_EXTERNAL_STORAGE", "android.permission.WRITE_EXTERNAL_STORAGE")]
-        [InlineData("CAMERA", "android.permission.CAMERA")]
-        [InlineData("ACCESS_FINE_LOCATION", "android.permission.ACCESS_FINE_LOCATION")]
-        public void SingleSegment_PrefixedWithAndroidPermission(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestPermissionGenerator.ResolveName(input));
-        }
-
-        [Theory]
-        [InlineData("partial.NAME", "android.permission.partial.NAME")]
-        [InlineData("a.b", "android.permission.a.b")]
-        public void TwoSegments_StillPrefixed(string input, string expected)
-        {
-            Assert.Equal(expected, AndroidManifestPermissionGenerator.ResolveName(input));
-        }
-
-        [Fact]
-        public void EmptyString_PrefixedWithAndroidPermission()
-        {
-            Assert.Equal("android.permission.", AndroidManifestPermissionGenerator.ResolveName(""));
-        }
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml([]));
     }
 
-    public class GenerateElementTests
+    [Fact]
+    public Task SingleShortName()
     {
-        [Fact]
-        public void BasicPermission_NoOptionalAttributes()
+        var permissions = new[]
         {
-            var perm = new AndroidPermission { Name = "WRITE_EXTERNAL_STORAGE" };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            Assert.Equal(
-                "<uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\" />",
-                xml
-            );
-        }
-
-        [Fact]
-        public void WithMaxSdkVersion()
-        {
-            var perm = new AndroidPermission
-            {
-                Name = "android.permission.READ_EXTERNAL_STORAGE",
-                MaxSdkVersion = "28"
-            };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            Assert.Contains("android:maxSdkVersion=\"28\"", xml);
-            Assert.StartsWith("<uses-permission", xml);
-            Assert.EndsWith("/>", xml);
-        }
-
-        [Fact]
-        public void WithMinSdkVersion()
-        {
-            var perm = new AndroidPermission
-            {
-                Name = "CAMERA",
-                MinSdkVersion = "21"
-            };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            Assert.Contains("android:minSdkVersion=\"21\"", xml);
-        }
-
-        [Fact]
-        public void WithBothSdkVersions()
-        {
-            var perm = new AndroidPermission
-            {
-                Name = "CAMERA",
-                MinSdkVersion = "21",
-                MaxSdkVersion = "33"
-            };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            Assert.Contains("android:minSdkVersion=\"21\"", xml);
-            Assert.Contains("android:maxSdkVersion=\"33\"", xml);
-        }
-
-        [Theory]
-        [InlineData(null)]
-        [InlineData("")]
-        [InlineData("   ")]
-        public void NullOrWhitespace_SdkVersions_OmittedFromOutput(string? value)
-        {
-            var perm = new AndroidPermission
-            {
-                Name = "CAMERA",
-                MinSdkVersion = value,
-                MaxSdkVersion = value
-            };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            Assert.DoesNotContain("minSdkVersion", xml);
-            Assert.DoesNotContain("maxSdkVersion", xml);
-        }
-
-        [Fact]
-        public void AttributeOrder_NameThenMinThenMax()
-        {
-            var perm = new AndroidPermission
-            {
-                Name = "CAMERA",
-                MinSdkVersion = "21",
-                MaxSdkVersion = "33"
-            };
-            var xml = AndroidManifestPermissionGenerator.GenerateElement(perm);
-
-            var nameIdx = xml.IndexOf("android:name");
-            var minIdx = xml.IndexOf("android:minSdkVersion");
-            var maxIdx = xml.IndexOf("android:maxSdkVersion");
-
-            Assert.True(nameIdx < minIdx);
-            Assert.True(minIdx < maxIdx);
-        }
+            new AndroidPermission { Name = "CAMERA" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
     }
 
-    public class GenerateManifestXmlTests
+    [Fact]
+    public Task SingleFullyQualifiedName()
     {
-        [Fact]
-        public void MatchesSpecExample()
+        var permissions = new[]
         {
-            var permissions = new[]
-            {
-                new AndroidPermission { Name = "android.permission.READ_EXTERNAL_STORAGE", MaxSdkVersion = "28" },
-                new AndroidPermission { Name = "WRITE_EXTERNAL_STORAGE" },
-                new AndroidPermission { Name = "some.namedpermissions.ACCESS" },
-            };
+            new AndroidPermission { Name = "android.permission.READ_EXTERNAL_STORAGE" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
+    }
 
-            var xml = AndroidManifestPermissionGenerator.GenerateManifestXml(permissions);
-
-            Assert.Contains("<manifest>", xml);
-            Assert.Contains("</manifest>", xml);
-            Assert.Contains("<uses-permission android:name=\"android.permission.READ_EXTERNAL_STORAGE\" android:maxSdkVersion=\"28\" />", xml);
-            Assert.Contains("<uses-permission android:name=\"android.permission.WRITE_EXTERNAL_STORAGE\" />", xml);
-            Assert.Contains("<uses-permission android:name=\"some.namedpermissions.ACCESS\" />", xml);
-        }
-
-        [Fact]
-        public void EmptyCollection_ValidManifestShell()
+    [Fact]
+    public Task WithMaxSdkVersion()
+    {
+        var permissions = new[]
         {
-            var xml = AndroidManifestPermissionGenerator.GenerateManifestXml([]);
+            new AndroidPermission { Name = "READ_EXTERNAL_STORAGE", MaxSdkVersion = "28" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
+    }
 
-            Assert.Contains("<manifest>", xml);
-            Assert.Contains("</manifest>", xml);
-            Assert.DoesNotContain("<uses-permission", xml);
-        }
-
-        [Fact]
-        public void SinglePermission_ValidOutput()
+    [Fact]
+    public Task WithMinSdkVersion()
+    {
+        var permissions = new[]
         {
-            var permissions = new[]
-            {
-                new AndroidPermission { Name = "CAMERA" },
-            };
+            new AndroidPermission { Name = "CAMERA", MinSdkVersion = "21" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
+    }
 
-            var xml = AndroidManifestPermissionGenerator.GenerateManifestXml(permissions);
-
-            Assert.Contains("<uses-permission android:name=\"android.permission.CAMERA\" />", xml);
-        }
-
-        [Fact]
-        public void Elements_AreIndentedWithFourSpaces()
+    [Fact]
+    public Task WithBothSdkVersions()
+    {
+        var permissions = new[]
         {
-            var permissions = new[]
-            {
-                new AndroidPermission { Name = "CAMERA" },
-            };
+            new AndroidPermission { Name = "CAMERA", MinSdkVersion = "21", MaxSdkVersion = "33" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
+    }
 
-            var xml = AndroidManifestPermissionGenerator.GenerateManifestXml(permissions);
-            var lines = xml.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            var permLine = lines.First(l => l.Contains("<uses-permission"));
-
-            Assert.StartsWith("    <uses-permission", permLine);
-        }
+    [Fact]
+    public Task MultiplePermissions_MixedNames()
+    {
+        var permissions = new[]
+        {
+            new AndroidPermission { Name = "android.permission.READ_EXTERNAL_STORAGE", MaxSdkVersion = "28" },
+            new AndroidPermission { Name = "WRITE_EXTERNAL_STORAGE" },
+            new AndroidPermission { Name = "some.namedpermissions.ACCESS" },
+            new AndroidPermission { Name = "CAMERA", MinSdkVersion = "21", MaxSdkVersion = "33" },
+        };
+        return Verify(AndroidManifestPermissionGenerator.GenerateManifestXml(permissions));
     }
 }
